4cec9c490ddd8a7a23e8ece9406908a063f7da99
1191430 <1191430@isep.ipp.pt>
2021-03-19 22:33:29 +0000

    Adicionado exercício 9 finalizado

--- pl1/ex9/Ex9.c
+++ pl1/ex9/Ex9.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <time.h>
+
+void func_print(int i)
+{
+    for(int j = (i*100) + 1; j<= (i*100) + 100; j++)
+    {
+        printf("Number: %d \n", j);
+    }
+}
 
 int main(void) {
 
-	pid_t parent;
-	int n = 100;
-	int status,j=1;
+    int status,j=0;
 
-	for(int i=0; i<10; i++) //Processo para criar os 10 processos filho
-	{
-		if(fork()==0)
-		{
-			for(j; j<=n;j++) // For para printar os 100 números de cada vez por filho.
-			{
-				printf("Number: %d \n",j);
-			}
+    for(int i=0; i<10; i++) //Processo para criar os 10 processos filho
+    {
+        if(fork()==0)
+        {
+            func_print(i);
+            printf("Process number: %d \n", i);
+            printf("\n");
 
-			exit(status); // Exit status para dar um valor para o processo pai esperar pelos processos filho.
-		}
+            exit(status); // Exit status para dar um valor para o processo pai esperar pelos processos filho.
+        }
+    }
 
-		j+=100;
-		n+=100;
-	}
+    for (int d=0; d<10; d++)
+    {
+    	wait(&status);
+    }
 
-	for(int d=0; d<10;d++)
-	{
-		waitpid(-1,&status,0);
-	}
+    printf("Process ended. Shutting down... \n");
 
-	// Não será possível manter a ordem, isto porque devido ao escalonamento não será possível saber qual o processo filho que está a ocorrer.
-	return 0;
+    // Não será possível manter a ordem, isto porque devido aos processos serem concorrentes não será possível saber qual o processo que é executado antes ou depois.
 }
