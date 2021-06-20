641f3bd4d305fdcba0ee7223507e2f6b5f77ca49
BarbaraDP <bdianapinto@outlook.com>
2021-06-20 12:54:21 +0100

    PL6 - Added ex3

---
+++ pl6/ex03/src/Account.java
@@ -0,0 +1,42 @@
+import java.util.concurrent.Semaphore;
+
+public class Account {
+    /**
+     * Protects operations on account
+     * Withdrawals and deposits are exclusive
+     */
+    private Semaphore semaphore;
+
+    final int accountNumber;
+    EurosAmount balance;
+
+    public Account(int accountNumber) {
+        this.accountNumber = accountNumber;
+        this.balance = new EurosAmount(100000);
+        this.semaphore = new Semaphore(1);
+    }
+
+    public boolean withdraw(EurosAmount amount) throws InterruptedException {
+        semaphore.acquire();
+        try {
+            balance.subtract(amount);
+        } catch (UnsupportedOperationException e) {
+            semaphore.release();
+            System.out.println(e.getMessage() + " Balance: " + balance + "    Withdrawal amount: " + amount);
+            return false;
+        }
+        semaphore.release();
+
+        System.out.printf("Withdrew %s from account %d. Current balance: %s\n", amount, accountNumber, balance);
+        return true;
+    }
+
+    public boolean deposit(EurosAmount amount) throws InterruptedException {
+        semaphore.acquire();
+        balance.add(amount);
+        semaphore.release();
+
+        System.out.printf("Deposited %s on account %d. Current balance: %s\n", amount, accountNumber, balance);
+        return true;
+    }
+}
---
+++ pl6/ex03/src/RequestTransfer.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+
+public class RequestTransfer extends Thread {
+    private Bank bank;
+    private Account from;
+    private Account to;
+    private EurosAmount amount;
+
+    public RequestTransfer(Bank bank, Account from, Account to, EurosAmount amount) {
+        this.bank = bank;
+        this.from = from;
+        this.to = to;
+        this.amount = amount;
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            sleep(new Random().nextInt(5000));
+            System.out.println("Initiating transfer " + getId() + " from account " + from.accountNumber + " to account " + to.accountNumber + " by " + amount);
+
+            if (bank.transferMoney(from, to, amount))
+                System.out.printf("Transfer %d successful\n", getId());
+            else
+                System.out.printf("Transfer %d rejected\n", getId());
+        } catch (InterruptedException e) {
+            System.out.println("Transfer was interrupted");
+            return;
+        }
+        return;
+    }
+}
